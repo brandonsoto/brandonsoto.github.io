538ba355d3d5d42a48869ed9814fe17def415034
Brandon Soto <brandon.soto09@gmail.com>
2017-01-08 18:49:24 -0800

    Merge javascript files into one

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,162 @@
-// TODO(brandon): need to move functions here
+$(function () {
+    "use strict";
 
-$(function() {
+    validate_contact_form();
+    setup_scrolling();
 
-});
+    $("a[data-toggle=\"tab\"]").click(function (e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function () {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    });
+
+    // Initialize and Configure Magnific Popup Lightbox Plugin
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a'
+        , type: 'image'
+        , tLoading: 'Loading image #%curr%...'
+        , mainClass: 'mfp-img-mobile'
+        , gallery: {
+            enabled: true
+            , navigateByImgClick: true
+            , preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        }
+        , image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
+
+    $("body").on("input propertychange", ".floating-label-form-group", function (e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function () {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function () {
+        $(this).removeClass("floating-label-form-group-with-focus");
+    });
+
+}); // end of document ready
+
+function setup_scrolling() {
+    "use strict";
+
+    // Initialize and Configure Scroll Reveal Animation
+    $('body').scrollspy({
+        target: '.navbar-fixed-top'
+        , offset: 51
+    });
+
+    window.sr = ScrollReveal();
+    sr.reveal('.sr-icons', {
+        duration: 600
+        , scale: 0.3
+        , distance: '0px'
+    }, 200);
+    sr.reveal('.sr-button', {
+        duration: 1000
+        , delay: 200
+    });
+    sr.reveal('.sr-contact', {
+        duration: 600
+        , scale: 0.3
+        , distance: '0px'
+    }, 300);
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+}
+
+function validate_contact_form() {
+    "use strict";
+
+    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function ($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function ($form, event) {
+            // Prevent spam click and default submit behaviour
+            $("#btnSubmit").attr("disabled", true);
+            event.preventDefault();
+
+            // get values from FORM
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var message = $("textarea#message").val();
+            var gotcha = $("input#gotcha").val();
+
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+
+            if (gotcha !== "") {
+                $('#success').html("<div class='alert alert-danger'>");
+                $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button>");
+                $('#success > .alert-danger').append("<strong>Sorry, this appears to be spam. It will not be sent. :(");
+                $('#success > .alert-danger').append('</div>');
+                //clear all fields
+                $('#contactForm').trigger("reset");
+                return;
+            }
+
+            $.ajax({
+                url: "https://formspree.io/brandon.soto09@gmail.com",
+                type: "POST",
+                data: {
+                    name: name,
+                    email: email,
+                    message: message
+                },
+                dataType: "json",
+                cache: false,
+                success: function () {
+                    // Enable button & show success message
+                    $("#btnSubmit").attr("disabled", false);
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function () {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                }
+            });
+        },
+        filter: function () {
+            return $(this).is(":visible");
+        }
+    });
+} // end of validate_contact_form
+
