3afc0d9738e8d4fc131260c726dfb9f8fb214206
Brandon Soto <brandon.soto09@gmail.com>
2017-09-06 21:17:50 -0700

    Update description and disable project section

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -142,7 +142,7 @@ function validate_contact_form() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Your message has been sent.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
